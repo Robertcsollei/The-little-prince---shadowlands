79763c287537223c17a48e195102b40603002744
Robert Csöllei <robertcsollei@gmail.com>
2020-10-01 10:30:31 +0200

    Update App

--- app/src/main/java/org/pondar/pacmankotlin/Game.kt
+++ app/src/main/java/org/pondar/pacmankotlin/Game.kt
@@ -3,8 +3,14 @@ package org.pondar.pacmankotlin
 import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
 import android.widget.TextView
+import java.lang.Math.floor
+import java.lang.Math.pow
 import java.util.ArrayList
+import kotlin.math.sqrt
 
 
 /**
@@ -14,23 +20,51 @@ import java.util.ArrayList
 
 class Game(private var context: Context,view: TextView) {
 
+        val mainHandler = Handler(Looper.getMainLooper())
+
+
+
         private var pointsView: TextView = view
         private var points : Int = 0
         //bitmap of the pacman
         var pacBitmap: Bitmap
-        var pacx: Int = 0
-        var pacy: Int = 0
+        var pacx: Double = 0.0
+        var pacy: Double = 0.0
 
+        var InitialX = 0
+        var InitialY = 0
+
+        var EndX = 0
+        var EndY = 0
+
+        var speed = 50
+
+        var acceleration:Double = 2.0
+        var accSpeed = 5.0
+
+        var descSpeed = 0.5
+
+        val updatePos = object : Runnable {
+            override fun run() {
+                speed += 2
+
+                mainHandler.postDelayed(this, 10)
+            }
+        }
 
         //did we initialize the coins?
         var coinsInitialized = false
 
         //the list of goldcoins - initially empty
         var coins = ArrayList<GoldCoin>()
+
+        var coinRadius = 80.0
+
+        var delCoin = -1
         //a reference to the gameview
         private var gameView: GameView? = null
-        private var h: Int = 0
-        private var w: Int = 0 //height and width of screen
+         var h: Int = 0
+         var w: Int = 0 //height and width of screen
 
 
     init {
@@ -38,22 +72,48 @@ class Game(private var context: Context,view: TextView) {
 
     }
 
+    fun accelerate(){
+        acceleration *= accSpeed
+    }
+
+    fun normalize() {
+        if(acceleration > 0.0){
+            acceleration -= descSpeed
+        }
+        if(acceleration < 0.0){
+            acceleration += descSpeed
+        }
+    }
+
     fun setGameView(view: GameView) {
         this.gameView = view
     }
 
     //TODO initialize goldcoins also here
-    fun initializeGoldcoins()
-    {
-        //DO Stuff to initialize the array list with coins.
-
+    fun initializeGoldcoins() {
         coinsInitialized = true
+
+        if(coinsInitialized) {
+
+            var counter = 0
+
+            var offsetX = 200
+            var offsetY = 200
+
+            for (x in 0..4) {
+                for (y in 0..4) {
+                    coins.add(GoldCoin(offsetX * x, offsetY * y, context, counter))
+                    counter++
+                }
+            }
+        }
+
     }
 
 
     fun newGame() {
-        pacx = 50
-        pacy = 400 //just some starting coordinates - you can change this.
+        pacx = 50.0
+        pacy = 400.0 //just some starting coordinates - you can change this.
         //reset the points
         coinsInitialized = false
         points = 0
@@ -64,22 +124,65 @@ class Game(private var context: Context,view: TextView) {
         this.h = h
         this.w = w
     }
+    fun resetPos () {
+        pacx = 50.0
+        pacy = 50.0
+        gameView!!.invalidate()
+    }
+
 
     fun movePacmanRight(pixels: Int) {
         //still within our boundaries?
+
+
         if (pacx + pixels + pacBitmap.width < w) {
-            pacx = pacx + pixels
-            doCollisionCheck()
+            acceleration += accSpeed
+            var newSpeed = pixels + acceleration
+            Log.d("speed", "$acceleration $accSpeed")
+            pacx += newSpeed.toInt()
+
             gameView!!.invalidate()
         }
+
     }
+
+    fun setPacPosition(x: Int, y: Int) {
+        if(!(EndX in 50 downTo -50 && EndY in 50 downTo -50)) {
+            var len = sqrt(pow(EndX.toDouble(), 2.0) + pow(EndY.toDouble(), 2.0))
+            Log.d("MATRIX", len.toString() + " " + (EndX / len).toString())
+            pacx += EndX / len * speed
+            pacy += EndY / len * speed
+        }
+
+        coins.forEachIndexed { index, goldCoin ->
+            doCollisionCheck(goldCoin, index)
+        }
+        if(delCoin > 0){
+            coins.removeAt(delCoin)
+            delCoin = -1
+        }
+        gameView!!.invalidate()
+        Log.d("Log", "gameView.toString()")
+
+    }
+
+
 
     //TODO check if the pacman touches a gold coin
     //and if yes, then update the neccesseary data
     //for the gold coins and the points
     //so you need to go through the arraylist of goldcoins and
     //check each of them for a collision with the pacman
-    fun doCollisionCheck() {
+    fun doCollisionCheck(coin: GoldCoin, index: Int) {
+
+        if(!coin.isCollected){
+            if(pacx in coin.goldX- coinRadius .. coin.goldX+coinRadius && pacy in coin.goldY-coinRadius.. coin.goldY+coinRadius){
+                coin.isCollected = true
+                delCoin = index
+                Log.d("Collected", coin.id.toString())
+
+            }
+        }
 
     }
 
--- app/src/main/java/org/pondar/pacmankotlin/GameView.kt
+++ app/src/main/java/org/pondar/pacmankotlin/GameView.kt
@@ -4,17 +4,32 @@ import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
+import android.os.Handler
+import android.os.Looper
 import android.util.AttributeSet
 import android.util.Log
+import android.view.MotionEvent
 import android.view.View
 
 
 //note we now create our own view class that extends the built-in View class
-class GameView : View {
+class GameView : View , View.OnTouchListener{
 
     private var game: Game? = null
     private var h: Int = 0
     private var w: Int = 0 //used for storing our height and width of the view
+
+    val mainHandler = Handler(Looper.getMainLooper())
+
+    val updatePos = object : Runnable {
+        override fun run() {
+            game?.speed = game?.speed?.plus(2)!!
+            Log.d("MATRIX", game?.speed.toString())
+            invalidate()
+
+            mainHandler.postDelayed(this, 10)
+        }
+    }
 
     fun setGame(game: Game?) {
         this.game = game
@@ -34,16 +49,21 @@ class GameView : View {
     //In the onDraw we put all our code that should be
     //drawn whenever we update the screen.
     override fun onDraw(canvas: Canvas) {
+
+        this.setOnTouchListener(this)
+
         //Here we get the height and weight
         h = canvas.height
         w = canvas.width
         //update the size for the canvas to the game.
         game?.setSize(h, w)
-        Log.d("GAMEVIEW", "h = $h, w = $w")
+
 
         //are the coins initiazlied?
         if (!(game!!.coinsInitialized))
             game?.initializeGoldcoins()
+
+
 
 
         //Making a new paint object
@@ -54,10 +74,45 @@ class GameView : View {
         canvas.drawBitmap(game!!.pacBitmap, game?.pacx!!.toFloat(),
                 game?.pacy!!.toFloat(), paint)
 
+
+        if(game!!.coinsInitialized){
+            for(coin in game?.coins!!){
+                canvas.drawBitmap(coin!!.goldBitmap, coin?.goldX!!.toFloat(),
+                        coin?.goldY!!.toFloat(), paint)
+            }
+        }
+
         //TODO loop through the list of goldcoins and draw them.
 
-        game?.doCollisionCheck()
+
         super.onDraw(canvas)
     }
 
+
+    override fun onTouch(v: View?, event: MotionEvent?): Boolean {
+
+        val x = event!!.x.toInt() -40
+        val y = event.y.toInt() -40
+
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+                game?.InitialX = x
+                game?.InitialY = y
+            }
+            MotionEvent.ACTION_MOVE -> Log.d("", "")
+            MotionEvent.ACTION_UP -> {
+               game?.EndX = x - game?.InitialX!!
+                game?.EndY = y - game?.InitialY!!
+                Log.i("TAG", "${game?.EndX} ${game?.EndY}")
+
+            game?.setPacPosition(x , y)
+
+                //mainHandler.post(updatePos)
+            }
+        }
+        invalidate()
+
+
+        return true
+    }
 }
